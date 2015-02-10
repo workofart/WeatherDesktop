616c8fd2f45de85f00b36460bf0f51e7d77de695
Zihao Wang <zwang688@uwo.ca>
2015-02-09 20:29:44 -0500

    Modified GitTut.java

--- gitTut/GitTut.java
+++ gitTut/GitTut.java
@@ -1,5 +1,5 @@
 public class GitTut{
   public static void main(String[] args){
-    System.out.println("Hello Git World\nMy username is zwang688");
+    System.out.println("Hello Git World\nMy username is zwang688 My\nname is Zihao Wang");
   }
 }
