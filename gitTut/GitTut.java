12e244870d7618b2d7c8a1ad25171b8acdf13ce6
Zihao Wang <zwang688@uwo.ca>
2015-02-09 20:36:21 -0500

    Prints email address

--- gitTut/GitTut.java
+++ gitTut/GitTut.java
@@ -1,5 +1,5 @@
 public class GitTut{
   public static void main(String[] args){
-    System.out.println("Hello Git World\nMy username is zwang688 My\nname is Zihao Wang");
+    System.out.println("Hello Git World\nMy email address is zwang688@uwo.ca\nMy name is Zihao Wang");
   }
 }
